54cdcbdb29ac533b9b351bab5378ee0c0cebfab8
Meja_ <meja_hedrenius@hotmail.com>
2025-08-29 15:04:41 +0200

    main program done

--- Myfirstapp/Program.cs
+++ Myfirstapp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+List<string> names = [];
+
+Console.Write("Ange ett namn:");
+names.Add(Console.ReadLine()!);
+
+Console.WriteLine("Namn i listan:");
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
+
+Console.WriteLine("Tryck på valfri tangent för att avsluta...");
+Console.ReadKey();
